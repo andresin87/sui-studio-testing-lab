5e0b70a1ded32b69a76264a6dd5b12cc5df4393f
andresin87 <andresin87@gmail.com>
2020-04-26 14:04:18 +0200

    feat(lab/giphCard): add tests

--- test/lab/giphCard/index.js
+++ test/lab/giphCard/index.js
@@ -1,6 +1,7 @@
 /* global LabGiphCard */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import chai, {expect} from 'chai'
 import chaiDOM from 'chai-dom'
@@ -9,8 +10,27 @@ import {render} from '@testing-library/react'
 chai.use(chaiDOM)
 
 describe('LabGiphCard', () => {
-  it('Render', () => {
-    render(<LabGiphCard />)
-    expect(true).to.be.eql(false)
+  it('should render without crashing', () => {
+    // Given
+    const props = {}
+
+    // When
+    const component = <LabGiphCard {...props} />
+
+    // Then
+    const div = document.createElement('div')
+    ReactDOM.render(component, div)
+    ReactDOM.unmountComponentAtNode(div)
+  })
+
+  it('should NOT render null', () => {
+    // Given
+    const props = {}
+
+    // When
+    const {container} = render(<LabGiphCard {...props} />)
+
+    // Then
+    expect(container).to.not.be.null
   })
 })
