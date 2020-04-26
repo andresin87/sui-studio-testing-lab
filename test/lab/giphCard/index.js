7750f92ebc65796c18c2324fa2d92727f73be67a
andresin87 <andresin87@gmail.com>
2020-04-26 14:25:02 +0200

    test(META): usage of debug

--- test/lab/giphCard/index.js
+++ test/lab/giphCard/index.js
@@ -28,9 +28,40 @@ describe('LabGiphCard', () => {
     const props = {}
 
     // When
-    const {container} = render(LabGiphCard, {...props})
+    const {container} = render(<LabGiphCard {...props} />)
 
     // Then
     expect(container).to.not.be.null
+  })
+
+  it('should render text and image', () => {
+    // Given
+    const props = {
+      url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
+      text: "I don't need debug my code I USE TDD"
+    }
+
+    // When
+    const {container, debug, ...otherTools} = render(<LabGiphCard {...props} />)
+    /** Priority [otherTools]
+     * - Queries Accessible to Everyone:
+     *      getByLabelText,
+     *      getByPlaceholderText,
+     *      getByRole,
+     *      getByText,
+     *      getByDisplayValue
+     * - Semantic Queries:
+     *      getByAltText,
+     *      getByTitle
+     * - Test IDs:
+     *      getByTestId
+     * **/
+    const {getByText, getByAltText} = otherTools
+
+    // Then
+    console.log(debug()) // Debug usage 😏
+    expect(getByText(props.text).innerText).to.be.equal(props.text)
+    expect(getByAltText(props.text).alt).to.be.equal(props.text)
+    expect(getByAltText(props.text).src).to.be.equal(props.url)
   })
 })
