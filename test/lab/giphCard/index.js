8e53ca9d0a5a6e85aacb7516350326b6017205ee
andresin87 <andresin87@gmail.com>
2020-04-27 21:50:11 +0200

    feat(lab/giphCard): mod demo rerender

--- test/lab/giphCard/index.js
+++ test/lab/giphCard/index.js
@@ -42,7 +42,7 @@ describe('LabGiphCard', () => {
     expect(container).to.not.be.null
   })
 
-  it('should render text and image', () => {
+  it('should render text and image', async () => {
     // Given
     const props = {
       url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
@@ -64,12 +64,37 @@ describe('LabGiphCard', () => {
      * - Test IDs:
      *      getByTestId
      * **/
-    const {getByText, getByAltText} = otherTools
+    const {
+      rerender,
+      getByText,
+      queryByText,
+      getByAltText,
+      queryByAltText
+    } = otherTools
 
     // Then
     console.log(debug()) // Debug usage 😏
     expect(getByText(props.text).innerText).to.be.equal(props.text)
     expect(getByAltText(props.text).alt).to.be.equal(props.text)
     expect(getByAltText(props.text).src).to.be.equal(props.url)
+
+    // But
+    // Given
+    const nextProps = {
+      url: 'https://media1.giphy.com/media/l6TD7USKX0QIo/giphy.gif',
+      text: 'I need to debug my code now'
+    }
+
+    // When
+    await rerender(<LabGiphCard {...nextProps} />)
+
+    // Then
+    expect(queryByText(props.text)).to.be.null
+    expect(queryByAltText(props.text)).to.be.null
+
+    // And
+    expect(getByText(nextProps.text).innerText).to.be.equal(nextProps.text)
+    expect(getByAltText(nextProps.text).alt).to.be.equal(nextProps.text)
+    expect(getByAltText(nextProps.text).src).to.be.equal(nextProps.url)
   })
 })
