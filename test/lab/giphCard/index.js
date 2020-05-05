3638b22dc558ea3d1d8c75cc40ec7b287fa7a38d
andresin87 <andresin87@gmail.com>
2020-05-05 18:40:36 +0200

    feat(Root): generateSetup

--- test/lab/giphCard/index.js
+++ test/lab/giphCard/index.js
@@ -11,26 +11,23 @@ import sinonChai from 'sinon-chai'
 import chaiDOM from 'chai-dom'
 import {fireEvent} from '@testing-library/react'
 
-import '@s-ui/studio/src/patcher-mocha'
-
-import '../../../patchContext'
+import '../../../generateSetup'
 
 chai.use(chaiDOM)
 chai.use(sinonChai)
 
-const setup = generateSetup(LabGiphCard)
-
 describe('LabGiphCard', () => {
+  const setup = generateSetup(LabGiphCard)
   it('should render without crashing', () => {
     // Given
     const props = {}
 
     // When
-    const component = <LabGiphCard {...props} />
+    const {Component} = setup(props)
 
     // Then
     const div = document.createElement('div')
-    ReactDOM.render(component, div)
+    ReactDOM.render(Component, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 
@@ -42,11 +39,129 @@ describe('LabGiphCard', () => {
     const {container} = setup(props)
 
     // Then
-    expect(container.innerHTML).to.be.a('string')
-    expect(container.innerHTML).to.not.have.lengthOf(0)
+    expect(container).to.not.be.null
   })
 
-  context(null, ({withContextProvider}) => {
+  it('should render text and image', async () => {
+    // Given
+    const props = {
+      url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
+      text: "I don't need debug my code I USE TDD"
+    }
+
+    // When
+    const {container, debug, Component, ...otherTools} = setup(props)
+    /** Priority [otherTools]
+     * - Queries Accessible to Everyone:
+     *      getByLabelText,
+     *      getByPlaceholderText,
+     *      getByRole,
+     *      getByText,
+     *      getByDisplayValue
+     * - Semantic Queries:
+     *      getByAltText,
+     *      getByTitle
+     * - Test IDs:
+     *      getByTestId
+     * **/
+    const {
+      rerender,
+      getByText,
+      queryByText,
+      getByAltText,
+      queryByAltText
+    } = otherTools
+
+    // Then
+    // console.log(debug()) // Debug usage 😏
+    expect(getByText(props.text).innerText).to.be.equal(props.text)
+    expect(getByAltText(props.text).alt).to.be.equal(props.text)
+    expect(getByAltText(props.text).src).to.be.equal(props.url)
+
+    // But
+    // Given
+    const nextProps = {
+      url: 'https://media1.giphy.com/media/l6TD7USKX0QIo/giphy.gif',
+      text: 'I need to debug my code now'
+    }
+
+    // When
+    await rerender(<Component {...nextProps} />)
+
+    // Then
+    expect(queryByText(props.text)).to.be.null
+    expect(queryByAltText(props.text)).to.be.null
+
+    // And
+    expect(getByText(nextProps.text).innerText).to.be.equal(nextProps.text)
+    expect(getByAltText(nextProps.text).alt).to.be.equal(nextProps.text)
+    expect(getByAltText(nextProps.text).src).to.be.equal(nextProps.url)
+  })
+
+  it('should fire onClickHandler event once', async () => {
+    // Given
+    const spy = sinon.spy()
+    const props = {
+      url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
+      text: "I don't need debug my code I USE TDD",
+      handleOnClick: spy
+    }
+
+    // When
+    const {getByText, getByAltText} = setup(props)
+
+    // Then
+    expect(spy).to.have.callCount(0)
+
+    // And
+    // When
+    fireEvent(
+      getByText(props.text),
+      new MouseEvent('click', {
+        bubbles: true, // if true, then the event bubbles.
+        cancelable: true // if true, then the “default action” may be prevented
+      })
+    )
+
+    // Then
+    expect(spy).to.have.callCount(0)
+
+    // But
+    // When
+    fireEvent.click(getByAltText(props.text))
+
+    // Then
+    expect(spy).to.have.callCount(1)
+
+    // Usage of assertions
+    // https://www.chaijs.com/plugins/sinon-chai/
+  })
+
+  describe('Component', LabGiphCard, setup => {
+    it('should render without crashing', () => {
+      // Given
+      const props = {}
+
+      // When
+      const {Component} = setup(props)
+
+      // Then
+      const div = document.createElement('div')
+      ReactDOM.render(Component, div)
+      ReactDOM.unmountComponentAtNode(div)
+    })
+
+    it('should NOT render null', () => {
+      // Given
+      const props = {}
+
+      // When
+      const {container} = setup(props)
+
+      // Then
+      expect(container).to.not.be.null
+    })
+
     it('should render text and image', async () => {
       // Given
       const props = {
@@ -55,10 +170,7 @@ describe('LabGiphCard', () => {
       }
 
       // When
-      const {container, debug, ...otherTools} = setup(
-        props,
-        withContextProvider
-      )
+      const {container, debug, Component, ...otherTools} = setup(props)
       /** Priority [otherTools]
        * - Queries Accessible to Everyone:
        *      getByLabelText,
@@ -94,7 +206,7 @@ describe('LabGiphCard', () => {
       }
 
       // When
-      await rerender(<LabGiphCard {...nextProps} />)
+      await rerender(<Component {...nextProps} />)
 
       // Then
       expect(queryByText(props.text)).to.be.null
@@ -145,10 +257,125 @@ describe('LabGiphCard', () => {
       // https://www.chaijs.com/plugins/sinon-chai/
     })
 
-    // Remembering why we test
-    // We write tests to be confident that our application will work when the USER uses them.
-    // Think less about the code you are testing and more about the use cases that code supports.
-    //   Code Coverage < Use Case Coverage (Kent C.Dots "How to know what to test" Apr.2019)
-    // <!-- Wrong KPIs can encourage developers to simply increase "vanity" metrics --> (Alex Lobera "Testing is all about principles" Dic.2019)
+    describe('default context', LabGiphCard, 'default', setup => {
+      it('should render without crashing', () => {
+        // Given
+        const props = {}
+
+        // When
+        const {Component} = setup(props)
+
+        // Then
+        const div = document.createElement('div')
+        ReactDOM.render(Component, div)
+        ReactDOM.unmountComponentAtNode(div)
+      })
+
+      it('should NOT render null', () => {
+        // Given
+        const props = {}
+
+        // When
+        const {container} = setup(props)
+
+        // Then
+        expect(container).to.not.be.null
+      })
+
+      it('should render text and image', async () => {
+        // Given
+        const props = {
+          url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
+          text: "I don't need debug my code I USE TDD"
+        }
+
+        // When
+        const {container, debug, Component, ...otherTools} = setup(props)
+        /** Priority [otherTools]
+         * - Queries Accessible to Everyone:
+         *      getByLabelText,
+         *      getByPlaceholderText,
+         *      getByRole,
+         *      getByText,
+         *      getByDisplayValue
+         * - Semantic Queries:
+         *      getByAltText,
+         *      getByTitle
+         * - Test IDs:
+         *      getByTestId
+         * **/
+        const {
+          rerender,
+          getByText,
+          queryByText,
+          getByAltText,
+          queryByAltText
+        } = otherTools
+
+        // Then
+        // console.log(debug()) // Debug usage 😏
+        expect(getByText(props.text).innerText).to.be.equal(props.text)
+        expect(getByAltText(props.text).alt).to.be.equal(props.text)
+        expect(getByAltText(props.text).src).to.be.equal(props.url)
+
+        // But
+        // Given
+        const nextProps = {
+          url: 'https://media1.giphy.com/media/l6TD7USKX0QIo/giphy.gif',
+          text: 'I need to debug my code now'
+        }
+
+        // When
+        await rerender(<Component {...nextProps} />)
+
+        // Then
+        expect(queryByText(props.text)).to.be.null
+        expect(queryByAltText(props.text)).to.be.null
+
+        // And
+        expect(getByText(nextProps.text).innerText).to.be.equal(nextProps.text)
+        expect(getByAltText(nextProps.text).alt).to.be.equal(nextProps.text)
+        expect(getByAltText(nextProps.text).src).to.be.equal(nextProps.url)
+      })
+
+      it('should fire onClickHandler event once', async () => {
+        // Given
+        const spy = sinon.spy()
+        const props = {
+          url: 'https://media1.giphy.com/media/Ok8r4XMtqTj3O/giphy.gif',
+          text: "I don't need debug my code I USE TDD",
+          handleOnClick: spy
+        }
+
+        // When
+        const {getByText, getByAltText} = setup(props)
+
+        // Then
+        expect(spy).to.have.callCount(0)
+
+        // And
+        // When
+        fireEvent(
+          getByText(props.text),
+          new MouseEvent('click', {
+            bubbles: true, // if true, then the event bubbles.
+            cancelable: true // if true, then the “default action” may be prevented
+          })
+        )
+
+        // Then
+        expect(spy).to.have.callCount(0)
+
+        // But
+        // When
+        fireEvent.click(getByAltText(props.text))
+
+        // Then
+        expect(spy).to.have.callCount(1)
+
+        // Usage of assertions
+        // https://www.chaijs.com/plugins/sinon-chai/
+      })
+    })
   })
 })
